ab3fc43ba30829c5264e38336612c2a5723775b7
jinho <g1063114@naver.com>
2022-01-12 21:52:45 +0900

    catalog data set

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-001', 'americano', 100, 2000);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-002', 'latte', 150, 2500);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-003', 'milk', 50, 1000);
